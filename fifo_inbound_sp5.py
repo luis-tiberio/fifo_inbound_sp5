b25c91b85043480ee9342df8d85686abd99badff
luis-tiberio <luisotaviotiberio@gmail.com>
2026-01-09 12:56:07 -0300

    Update fifo_inbound_sp5.py

--- fifo_inbound_sp5.py
+++ fifo_inbound_sp5.py
@@ -156,7 +156,7 @@ async def main():
                 pass
 
             print("Exportando...")
-            await page.locator('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[8]/div/span/div[2]/span/span/span/button').click()
+            await page.locator('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[8]/div/span/div[2]/span/span').click()
             # await page.locator("span").filter(has_text="Exportar").nth(2).click()
             # await page.get_by_role('button', name='Exportar').click(force=True) #ok
             await page.wait_for_timeout(5000)
